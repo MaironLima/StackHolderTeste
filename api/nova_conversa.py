a8f8627c2e5c9289e6820785bd5fc70262741140
Mairon Lima <maironlmelo@gmail.com>
2026-09-16 15:51:26 -0300

    .

--- api/nova_conversa.py
+++ api/nova_conversa.py
@@ -5,7 +5,6 @@ app.secret_key = "um_segredo_aleatorio_para_sessoes"
 
 
 @app.route("/api/nova-conversa", methods=["POST"])
-@app.route("/api/nova_conversa", methods=["POST"])
 def nova_conversa():
     session.clear()
     return jsonify({"mensagem": "Nova conversa iniciada."})
